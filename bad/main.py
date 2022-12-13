3f179a5a1c1822ebac2b78cd245bb58937e03d9c
Alexander Bleissem <alexander@bleissem.com>
2022-12-13 22:45:14 +0100

    add runner class

--- bad/main.py
+++ bad/main.py
@@ -8,10 +8,14 @@ sys.path.append(parentPath)
 from hanabi_learning_environment.agents.simple_agent import SimpleAgent
 
 from badagent import Bad_Agent
+from runner import Runner
 
 
 if __name__ == '__main__':
     
+    runner = Runner()
+    runner.Run()
+
     flags = {'players': 2, 'num_episodes': 1, 'agent_class': 'HatAgent'}
 
     agent1 = SimpleAgent({'players': flags['players']}, 'Agent1')
---
+++ bad/runner.py
@@ -0,0 +1,6 @@
+class Runner:
+    def __init__(self):
+        pass
+
+    def Run(self):
+        pass
