0de7bfde72be72d9e4e7fd6345d69da89aa0baf9
Alexander Bleissem <alexander@bleissem.com>
2022-12-13 22:48:58 +0100

    adjust setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 from skbuild import setup
 
 setup(
-    name='hanabi_learning_environment',
-    version='0.0.1',
-    description='Learning environment for the game of hanabi.',
-    author='deepmind/hanabi-learning-environment',
-    packages=['hanabi_learning_environment', 'hanabi_learning_environment.agents'],
-    install_requires=['cffi']
+    name='ws2223-group7-hanabi-learning-environment-alexb',
+    version='0.0.1',    
+    description='ws2223-group7 Learning environment for the game of hanabi for alexb.',
+    long_description='ws2223-group7 Learning environment for the game of hanabi for alexb.',
+    long_description_content_type="text/markdown",
+    author='ws2223-group7/hanabi-learning-environment',
+    packages=['hanabi_learning_environment', 'hanabi_learning_environment.agents', 'bad'],
+    install_requires=['cffi'],
+    python_requires=">=3.6",
+    classifiers = [
+    "Programming Language :: Python :: 3",
+    "Operating System :: POSIX :: Linux",
+    ]    
 )
